8242eab5fff0751195f8aa80a5a45fa02c886610
Paul M Dorr <paul.mdorr@gmail.com>
2016-08-23 22:40:48 -0300

    Add date range filter

--- js/umamiHappyReportApp.js
+++ js/umamiHappyReportApp.js
@@ -8,6 +8,7 @@ umamiHappyReportApp.controller('BoardMembersListController', function BoardMembe
         });
         Trello.rest('GET', 'boards/'+$scope.currentBoard.id+'/actions', function(actions) {
             $scope.actions = actions;
+            $scope.updateDateRange();
             $scope.$apply();
         });
     }
@@ -21,4 +22,26 @@ umamiHappyReportApp.controller('BoardMembersListController', function BoardMembe
 
     $scope.startDate = null;
     $scope.endDate = null;
+
+    $scope.updateDateRange = function() {
+        if ($scope.startDate != null && $scope.endDate != null) {
+            if ($scope.endDate <= $scope.startDate) {
+                console.log('Start date: '+$scope.startDate);
+                console.log('End date: '+$scope.endDate);
+                $scope.endDate.setDate($scope.startDate.getDate() + 1);
+                console.log('Start date: '+$scope.startDate);
+                console.log('End date: '+$scope.endDate);
+            }
+            $scope.filteredActions = [];
+            for (action in $scope.actions) {
+                var actionDate = new Date($scope.actions[action].date);
+                if ($scope.actions[action].data != undefined && $scope.actions[action].data.card != undefined &&
+                  actionDate >= $scope.startDate && actionDate <= $scope.endDate) {
+                    $scope.filteredActions.push($scope.actions[action]);
+                }
+            }
+        } else {
+            $scope.filteredActions = $scope.actions;
+        }
+    }
 });
